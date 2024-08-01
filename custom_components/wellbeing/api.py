20f90a175a305939d0aa8e531ae4a104bdb5baee
Johan Nenzén <johan.nanzen@gmail.com>
2024-08-02 01:35:17 +0200

    Update custom_components/wellbeing/api.py

--- custom_components/wellbeing/api.py
+++ custom_components/wellbeing/api.py
@@ -297,7 +297,6 @@ class WellbeingApiClient:
         headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
-            # "User-Agent": USER_AGENT
         }
         return await self.api_wrapper("post", TOKEN_URL, json, headers)
 
