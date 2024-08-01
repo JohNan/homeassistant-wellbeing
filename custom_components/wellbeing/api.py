b3fac684c1c52dbe212a9125a895098ca06fb55d
Johan Nenzén <johan.nanzen@gmail.com>
2024-08-02 01:35:46 +0200

    Update custom_components/wellbeing/api.py

--- custom_components/wellbeing/api.py
+++ custom_components/wellbeing/api.py
@@ -340,7 +340,6 @@ class WellbeingApiClient:
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json",
             "Accept": "application/json",
-            # "User-Agent": USER_AGENT,
             "x-api-key": X_API_KEY
         }
         url = f"{APPLIANCES_URL}/{pnc_id}/info"
