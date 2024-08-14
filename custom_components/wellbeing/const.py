663a4c4f635c3a63cc8ba05b260ff552e311ee9d
Johan Nenzén <johan.nanzen@gmail.com>
2024-08-14 21:44:14 +0200

    Code clean up and refactor

--- custom_components/wellbeing/const.py
+++ custom_components/wellbeing/const.py
@@ -1,28 +1,14 @@
 """Constants for Wellbeing."""
+
 # Base component constants
 CONFIG_FLOW_TITLE = "Electrolux Wellbeing"
 NAME = "Wellbeing"
 DOMAIN = "wellbeing"
-DOMAIN_DATA = f"{DOMAIN}_data"
-
 # Icons
 ICON = "mdi:format-quote-close"
 
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
-
-# Platforms
-BINARY_SENSOR = "binary_sensor"
-SENSOR = "sensor"
-SWITCH = "switch"
-FAN = "fan"
-SWITCH = "switch" 
-PLATFORMS = [SENSOR, FAN, BINARY_SENSOR, SWITCH]
-
 # Configuration and options
 CONF_ENABLED = "enabled"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
 CONF_SCAN_INTERVAL = "scan_interval"
 CONF_REFRESH_TOKEN = "refresh_token"
 
