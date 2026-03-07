7f3ea86e950d377f542b4ff26b3d627e6dbfe26d
Joakim Jaldén <jalden@kth.se>
2026-03-07 18:27:18 +0100

    Implemented clean_area functionality for the Purei9

--- custom_components/wellbeing/config_flow.py
+++ custom_components/wellbeing/config_flow.py
@@ -110,9 +110,7 @@ class WellbeingFlowHandler(ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
                     vol.Required(CONF_REFRESH_TOKEN): str,
                 }
             ),
-            description_placeholders={
-                "docs_url": "https://github.com/JohNan/homeassistant-wellbeing"
-            },
+            description_placeholders={"docs_url": "https://github.com/JohNan/homeassistant-wellbeing"},
             errors=self._errors,
         )
 
